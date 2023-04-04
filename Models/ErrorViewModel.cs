499b3650bd76bd92d3aceb8e6ac10737de603c75
Ulisses Queiroz da Silva <62977318+UlissesDSilva@users.noreply.github.com>
2023-04-03 22:56:35 -0300

    Start project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectWebApiDotnet.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
